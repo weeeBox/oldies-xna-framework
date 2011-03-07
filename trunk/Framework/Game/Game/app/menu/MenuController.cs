1b4d30c38c199176fc94b51502e1ea9be7152fe1
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-03-07 21:25:18 +0000

    Fixes

--- trunk/Framework/Game/Game/app/menu/MenuController.cs
+++ trunk/Framework/Game/Game/app/menu/MenuController.cs
@@ -11,21 +11,21 @@ namespace app.menu
      {        
         private LoadingState loadingState;        
         
-        private static readonly String[] STARTUP_RESOURCES = new String[]{ "poke_logo.png" , "logo_pp.png" };
+        private static readonly String[] STARTUP_RESOURCES = new String[] {};
         
-        private static readonly String[] COMMON_RESOURCES = new String[]{ "font_menu.fnt" , "font_menu_mini.fnt" , "gems_info.png" , "button_regular_passive.png" , "button_regular_active.png" , "button_big_active.png" , "button_big_passive.png" , "gems_back.png" , "gems_other.png" , "gems_play.png" , "gems_more_games.png" , "gems_open_feint.png" , "gems_play_main.png" , "point_passive.png" , "point_active.png" , "lock.png" , "of_logo.png" , "menu_background.png" , "strings.str" };
+        private static readonly String[] COMMON_RESOURCES = new String[] {};
         
-        private static readonly String[] RESOURCES_FULL = new String[]{ "logo.png" };
+        private static readonly String[] RESOURCES_FULL = new String[] {};
         
-        private static readonly String[] RESOURCES_FREE = new String[]{ "logo_free.png" };
+        private static readonly String[] RESOURCES_FREE = new String[] {};
         
-        private static readonly String[] MENU_RESOURCES = new String[]{ "font_head.fnt" , "actions_done.png" , "actions_lock.png" , "actions_play.png" , "actions_select.png" , "frenzy.png" , "arrow_active.png" , "arrow_passive.png" , "full_ver_picture.png" , "full_ver_text.png" , "how_to_play_01.png" , "how_to_play_02.png" , "how_to_play_03.png" , "how_to_play_04.png" };
+        private static readonly String[] MENU_RESOURCES = new String[] {};
         
-        private static readonly String[] GAME_RESOURCES = new String[]{ "popup_background.png" , "mission_done.png" , "mission_lock.png" , "mission_select.png" , "key.png" , "back.parts" , "blue.parts" , "bonus.parts" , "static.parts" , "default.parts" , "generator.parts" , "green.parts" , "purple.parts" , "red.parts" , "yellow.parts" , "black.parts" , "white.parts" , "orange.parts" , "colorless.parts" , "colorless1.parts" , "hud.parts" , "star_anim.ani" , "destroy_hex.ani" , "destroy_tri_1.ani" , "destroy_tri_2.ani" , "combine_hex.ani" , "combine_tri_1.ani" , "combine_tri_2.ani" , "combine_hex_colorless.ani" , "combine_tri_1_colorless.ani" , "combine_tri_2_colorless.ani" , "hex_colorless.ani" , "tri_1_colorless.ani" , "tri_2_colorless.ani" , "purple_anim_1.ani" , "purple_anim_2.ani" , "purple_anim_3.ani" , "red_anim_1.ani" , "red_anim_2.ani" , "red_anim_3.ani" , "yellow_anim_1.ani" , "yellow_anim_2.ani" , "yellow_anim_3.ani" , "blue_anim_1.ani" , "blue_anim_2.ani" , "blue_anim_3.ani" , "green_anim_1.ani" , "green_anim_2.ani" , "green_anim_3.ani" , "black_anim_1.ani" , "black_anim_2.ani" , "black_anim_3.ani" , "white_anim_1.ani" , "white_anim_2.ani" , "white_anim_3.ani" , "orange_anim_1.ani" , "orange_anim_2.ani" , "orange_anim_3.ani" , "generator.ani" , "drop_anim.ani" , "drop_destroy.ani" , "wings_anim.ani" , "wings_destroy.ani" , "main_destroy_1.ani" , "main_destroy_2.ani" , "hud.ani" , "hud_progress.ani" , "hud_score_bar.ani" , "hud_time_bar.ani" , "man_back.ani" , "man_run_right.ani" , "man_stand_left.ani" , "man_walk_left.ani" , "star.ani" , "star_destroy.ani" , "fire_anim.ani" , "main_turnover_1.ani" , "main_turnover_2.ani" , "obstacle_1.ani" , "obstacle_2.ani" , "main_rotate.ani" , "cell_marker_1.ani" , "cell_marker_2.ani" , "cell_way_1.ani" , "cell_way_2.ani" , "cell_selected_1.ani" , "cell_selected_2.ani" , "main_select_1.ani" , "main_select_2.ani" , "tap_here.ani" , "tap_here_tutorial.ani" , "win_effect.png" , "win_effect1.png" , "icicle.png" , "icicle_boost.png" , "pause_button_on.png" , "pause_button_off.png" , "star.png" , "star_ingame.png" , "level_counter_back.png" , "level_counter_progress.png" , "score_icon.png" , "time_up.png" , "font_score.fnt" , "font_score2.fnt" , "green_splash.png" , "pink_combo_wings.png" , "yellows_on.png" , "blue_freeze.png" , "black_exchange.png" , "time_bonus.png" , "caffeine.png" , "workaholic.png" , "arrow.png" , "hex_black_exchange.png" , "tri_1_black_exchange.png" , "tri_2_black_exchange.png" , "hex_blue_freeze.png" , "tri_1_blue_freeze.png" , "tri_2_blue_freeze.png" , "hex_green_splash.png" , "tri_1_green_splash.png" , "tri_2_green_splash.png" , "hex_pink_combo_wings.png" , "tri_1_pink_combo_wings.png" , "tri_2_pink_combo_wings.png" , "hex_yellows_on.png" , "tri_1_yellows_on.png" , "tri_2_yellows_on.png" , "hex_workaholic.png" , "tri_1_workaholics.png" , "tri_2_workaholic.png" };
+        private static readonly String[] GAME_RESOURCES = new String[] {};
         
-        private static readonly String[] GAME_RESOURCES_FULL = new String[]{ "time_up_effect_1.png" , "time_up_effect_2.png" };
+        private static readonly String[] GAME_RESOURCES_FULL = new String[] {};
         
-        private static readonly String[] GAME_RESOURCES_FREE = new String[]{ "time_up_effect_1_free.png" , "time_up_effect_2_free.png" };
+        private static readonly String[] GAME_RESOURCES_FREE = new String[] {};
         
         public MenuController() 
         {
@@ -55,8 +55,6 @@ namespace app.menu
                 case LoadingState.APP:                    
                 {
                     LoadResourcesOnStart();                    
-                    Sounds.LoadFullEnvironment();
-                    Sounds.PlayMenuMusic();
                     UnloadPack(STARTUP_RESOURCES);
                     StartScreen(ScreenFactory.CreateMainMenu(this));                    
                 }
--- trunk/Framework/Game/Game/app/menu/ScreenFactory.cs
+++ trunk/Framework/Game/Game/app/menu/ScreenFactory.cs
@@ -7,6 +7,7 @@ using app;
 using asap.ui;
 using asap.core;
 using asap.graphics;
+using asap.app;
 
 namespace app.menu
 {
@@ -18,37 +19,14 @@ namespace app.menu
         public static Screen CreateMainMenu(MenuListener listener)
         {
             Screen screen = new Screen(ScreenId.MAIN_MENU);
-            ScreenFactory.CreateDefaultMenuScreen(screen, null);
-            Image headerImage = ScreenFactory.GetImage((Config.freeVersion ? "logo_free.png" : "logo.png"));
-            ImageView header = new ImageView(headerImage);
-            screen.AddView(header);
-            screen.AlignCenter(header);
-            Container container = new Container();
-            container.AddView(new MenuButton(ButtonType.PLAY_FRENZY , ButtonId.START_FRENZY , listener));
-            container.AddView(new MenuButton(ButtonType.PLAY_CHALLENGE , ButtonId.START_CHALLENGE , listener));
-            ScreenFactory.AddAndLayoutButtons(screen, container, 150);
-            container = new Container();            
-            ScreenFactory.AddAndLayoutButtons(screen, container, 310);
-            screen.SetBackListener(listener, ButtonId.APP_EXIT);
+            screen.AddView(new ColorRect(screen.GetWidth(), screen.GetHeight(), 0xffffff));
             return screen;
         }
 
         public static Screen CreateStartLoading(MenuListener listener)
         {
             Screen screen = new Screen(ScreenId.START_LOADING);
-            screen.AddView(new ImageView(ScreenFactory.GetImage("menu_background.png")));
-            Container content = new Container();
-            content.SetSize(screen.GetWidth(), screen.GetHeight());
-            content.AddView(new ImageView(ScreenFactory.GetImage("poke_logo.png")));
-            Container spacer = new Container();
-            content.AddView(spacer);            
-            spacer = new Container();
-            spacer.SetSize(0, 10);
-            content.AddView(spacer);
-            content.AddView(new TextBox("Loading", ScreenFactory.GetFont("font_menu_mini.fnt"), ((screen.GetWidth()) - 20)));
-            content.SpreadVertically();
-            content.AlignCenterAll();
-            screen.AddView(content);
+            screen.AddView(new ColorRect(screen.GetWidth(), screen.GetHeight(), 0xffffff));
             return screen;
         }
         
@@ -68,7 +46,10 @@ namespace app.menu
         
         public static void CreateDefaultMenuScreen(Screen screen, String header)
         {
-            ImageView back = new ImageView(ScreenFactory.GetImage("menu_background.png"));
+            App app = App.GetInstance();
+
+            // ImageView back = new ImageView(ScreenFactory.GetImage("menu_background.png"));
+            ColorRect back = new ColorRect(app.GetWidth(), app.GetHeight(), 0xffffff);
             screen.AddView(back);
             if (header != null) 
             {
