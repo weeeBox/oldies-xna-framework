b589b674ae090663a8958431c5b0f0f1c7e6da5e
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-04-10 19:25:29 +0000

    Progress

--- trunk/Framework/Game/Game/app/menu/MainScreen.cs
+++ trunk/Framework/Game/Game/app/menu/MainScreen.cs
@@ -7,25 +7,28 @@ using asap.graphics;
 using AsapXna.asap.anim;
 using app;
 using asap.anim;
+using asap.core;
 
 namespace Game.app.menu
 {
     public class MainScreen : Screen
     {
-        private SwfPlayer player;
+        private SwfPlayer player;               
 
         public MainScreen() : base(ScreenId.MAIN_MENU)
         {
             SwfMovie movie = (SwfMovie)AppResManager.GetInstance().GetRes("test.swp");
             player = new SwfPlayer();
             player.SetMovie(movie);
-        }
+
+            player.Start(this);
+        }        
 
         public override void Draw(Graphics g)
         {
             base.Draw(g);
 
             player.Draw(g);
-        }
+        }        
     }
 }
