d201c1d66d29197aff450f69f27426527fcb31f9
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-04-10 13:32:38 +0000

    Progress

---
+++ trunk/trunk/Framework/AsapXna/asap/sound/Mp3SoundPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Media;
+using asap.sound;
+
+namespace asap.sound
+{
+    public class Mp3SoundPlayer : SoundPlayer
+    {
+        private Song song;
+
+        public Mp3SoundPlayer(Song song, string filename) : base(filename)
+        {
+            this.song = song;
+        }
+
+        public override void Play(bool looped)
+        {
+            MediaPlayer.Stop();
+            MediaPlayer.IsRepeating = looped;
+            MediaPlayer.Play(song);
+        }
+
+        public override void Pause()
+        {
+            if (IsPlaying())
+            {
+                MediaPlayer.Pause();
+            }
+        }
+
+        public override void Resume()
+        {
+            if (IsPaused())
+            {
+                MediaPlayer.Play(song);
+            }
+        }
+
+        public override void Stop()
+        {
+            if (IsPlaying() || IsPaused())
+            {
+                MediaPlayer.Stop();
+            }
+        }
+
+        public override bool IsPaused()
+        {
+            return MediaPlayer.State == MediaState.Paused;
+        }
+
+        public override bool IsPlaying()
+        {
+            return MediaPlayer.State == MediaState.Playing;
+        }
+    }
+}
