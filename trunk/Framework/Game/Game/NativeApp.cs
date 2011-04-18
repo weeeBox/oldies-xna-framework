75d464b02e493d1b80cc01e50ac6fc5f024e83e5
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-04-18 20:57:53 +0000

    Add some missed classes

---
+++ trunk/Framework/Game/Game/NativeApp.cs
@@ -0,0 +1,66 @@
+﻿using asap.app;
+using asap.core;
+using asap.graphics;
+using asap.resources;
+using Microsoft.Xna.Framework.Content;
+
+namespace app
+{
+    public class NativeApp : AppImpl
+    {
+        private Application application;        
+        private bool running;
+
+        public NativeApp(int width, int height, ContentManager content)
+        {
+            new ResFactory(content);            
+            new TextureManager(content);
+
+            application = new Application(width, height);
+            application.SetImpl(this);            
+            running = true;
+
+            application.Start();
+        }        
+        
+        public void Stop()
+        {
+            running = false;
+        }                
+
+        public void Tick(float deltaTime)
+        {
+            application.Tick(deltaTime);
+        }
+
+        public void PointerPressed(int x, int y)
+        {
+            application.PointerPressed(x, y, 0);
+        }
+
+        public void PointerDragged(int x, int y)
+        {
+            application.PointerDragged(x, y, 0);
+        }
+
+        public void PointerReleased(int x, int y)
+        {
+            application.PointerReleased(x, y, 0);
+        }
+
+        public void BackPressed()
+        {
+            application.KeyPressed(KeyCode.CANCEL, KeyAction.NONE);
+        }
+
+        public void Draw(Graphics g)
+        {           
+            application.Draw(g);           
+        }
+
+        public bool isRunning()
+        {
+            return running;
+        }
+    }
+}
---
+++ trunk/Framework/Game/Game/app/StartupController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using asap.core;
+using asap.resources;
+using System.Diagnostics;
+
+namespace app
+{
+    public class StartupController : Controller, ResourceMgrListener
+    {
+        public StartupController()
+        {
+
+        }
+
+        public override void Start(int param)
+        {
+            base.Start(param);
+
+            Application.sharedResourceMgr.LoadPacks(this, ResPacks.PACK_COMMON1);
+        }
+
+        public void resourceLoaded(ResourceLoadInfo res)
+        {
+            Debug.WriteLine("Resource loaded: " + res.fileName);
+        }
+
+        public void allResourcesLoaded()
+        {
+            Debug.WriteLine("Resources loaded");            
+        }
+    }
+}
