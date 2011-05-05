9bb397aeedfed718bbb6de4241f33cf7c192439e
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-05-05 23:40:30 +0000

    Stuff

--- trunk/trunk/Framework/Game/Game/app/Res.cs
+++ trunk/trunk/Framework/Game/Game/app/Res.cs
@@ -14,11 +14,14 @@ namespace app
 	public class Res
 	{
 		// PACK_COMMON
-		public const int IMG_UI_BUTTON_A = 0;
-		public const int IMG_UI_BUTTON_B = 1;
+		public const int ATLAS_COMMON = 0;
+		public const int IMG_UI_BUTTON_A = 1;
+		public const int IMG_UI_BUTTON_B = 2;
 		// PACK_MENU
-		public const int ANI_SWF_TEST = 2;
-		public const int RES_COUNT = 3;
+		public const int ANI_SWF_TEST = 3;
+		public const int IMG_UI_BUTTON_A2 = 4;
+		public const int IMG_UI_BUTTON_B2 = 5;
+		public const int RES_COUNT = 6;
 	}
 	
 	public class Resources
@@ -28,13 +31,14 @@ namespace app
 			// PACK_COMMON
 			new ResourceLoadInfo[]
 			{
-				new ResourceLoadInfo("buttonA", Res.IMG_UI_BUTTON_A, ResType.IMAGE),
-				new ResourceLoadInfo("buttonB", Res.IMG_UI_BUTTON_B, ResType.IMAGE),
+				new ResourceLoadInfo("common", Res.ATLAS_COMMON, ResType.ATLAS),
 			},
 			// PACK_MENU
 			new ResourceLoadInfo[]
 			{
 				new ResourceLoadInfo("test", Res.ANI_SWF_TEST, ResType.SWF),
+				new ResourceLoadInfo("buttonA", Res.IMG_UI_BUTTON_A2, ResType.IMAGE),
+				new ResourceLoadInfo("buttonB", Res.IMG_UI_BUTTON_B2, ResType.IMAGE),
 			},
 		};
 	}
