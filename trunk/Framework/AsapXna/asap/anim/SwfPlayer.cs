12fb54264c504a728ac6b5a8897829961e5df835
a.lementuev <a.lementuev@e87bcf64-a301-f259-9672-7f34c6d520c2>
2011-06-02 20:55:02 +0000

    Step by step fixes

--- trunk/Framework/AsapXna/asap/anim/SwfPlayer.cs
+++ trunk/Framework/AsapXna/asap/anim/SwfPlayer.cs
@@ -25,7 +25,7 @@ namespace asap.anim
         {
             STOPPED,
             PLAYING,
-            PAUSED
+            STEP
         };        
 
         private SwfMovie movie;
@@ -42,6 +42,8 @@ namespace asap.anim
         private int frameRate;
         private SWFFrame[] frames;
 
+        private bool forward;
+
         private SwfPlayerCache instanceCache;
         private DisplayObjectContainer displayList;
 
@@ -50,6 +52,7 @@ namespace asap.anim
             this.displayList = displayList;
             animationType = AnimationType.NORMAL;
             instanceCache = new SwfPlayerCache();
+            forward = true;
         }
 
         public void SetMovie(SwfMovie movie)
@@ -67,7 +70,7 @@ namespace asap.anim
         {
             state = PlayerState.STOPPED;
             
-            currentFrame = -1;
+            currentFrame = forward ? -1 : framesCount;
             frameElaspedTime = 0;            
             displayList.RemoveAllChilds();
         }                
@@ -79,23 +82,87 @@ namespace asap.anim
                 frameElaspedTime += delta;                                                               
                 if (frameElaspedTime > frameDelay)
                 {
-                    NextFrame();
+                    EnterFrame();
                     frameElaspedTime = 0;
                 }                
             }    
-        }        
+        }
+
+        private void EnterFrame()
+        {
+            if (forward)
+            {
+                EnterNextFrame();
+            }
+            else
+            {
+                EnterPrevFrame();
+            }
+        }
+
+        private void EnterNextFrame()
+        {
+            currentFrame++;
+
+            // Debug.WriteLine("Next: " + currentFrame);
+
+            Tag[] tags = GetCurrentFrame().Tags;
+            for (int i = 0; i < tags.Length; ++i)
+            {
+                ProcessTag(tags[i], FRAME_PROCESS_FORWARD);
+            }
+
+            if (currentFrame == framesCount - 1)
+            {
+                OnAnimationFinished();
+            }
+        }
+
+        private void EnterPrevFrame()
+        {            
+            Tag[] tags;
+            if (currentFrame < FramesCount && GetCurrentFrame().IsDispListChange())
+            {
+                tags = GetCurrentFrame().Tags;
+                for (int i = tags.Length - 1; i >= 0; --i)
+                {
+                    ProcessTag(tags[i], FRAME_PROCESS_CLEAR);
+                }
+            }
+
+            currentFrame--;
+
+            // Debug.WriteLine("Prev: " + currentFrame);
+
+            tags = GetCurrentFrame().Tags;
+            for (int i = tags.Length - 1; i >= 0; --i)
+            {
+                ProcessTag(tags[i], FRAME_PROCESS_BACKWARD);
+            }
+
+            if (currentFrame == 0)
+            {
+                OnAnimationFinished();
+            }
+        }
 
         protected virtual void OnAnimationFinished()
         {
+            if (state == PlayerState.STEP)
+                return;
+
             Stop();
 
             switch (animationType)
             {
-                case AnimationType.NORMAL:
+                case AnimationType.NORMAL:                    
                     break;
                 case AnimationType.LOOP:
+                {
+                    Reset();
                     Play();
                     break;
+                }
                 case AnimationType.PING_PONG:
                     throw new NotImplementedException();
                 default:
@@ -107,8 +174,14 @@ namespace asap.anim
         // Tags logic
         /////////////////////////////////////////////////////////////////////////////
 
-        private void ProcessTag(Tag tag, bool forward)
+        private const int FRAME_PROCESS_FORWARD = 0;
+        private const int FRAME_PROCESS_BACKWARD = 1;
+        private const int FRAME_PROCESS_CLEAR = 2;
+
+        private void ProcessTag(Tag tag, int mode)
         {
+            // Debug.WriteLine("\t" + tag);
+
             switch (tag.GetCode())
             {
                 case TagConstants.PLACE_OBJECT:
@@ -119,19 +192,19 @@ namespace asap.anim
                     }
                 case TagConstants.PLACE_OBJECT_2:
                     {
-                        doPlaceObject2(tag, forward);
+                        doPlaceObject2(tag, mode);
                         break;
                     }
                 case TagConstants.REMOVE_OBJECT:
                 case TagConstants.REMOVE_OBJECT_2:
                     {                    
-                        DoRemoveObject(tag, forward);                 
+                        DoRemoveObject(tag, mode);                 
                         break;
                     }
             }
         }        
 
-        private void doPlaceObject2(Tag tag, bool forward)
+        private void doPlaceObject2(Tag tag, int mode)
         {
             PlaceObject2 placeObject = (PlaceObject2)tag;
             bool isMove = placeObject.IsMove();
@@ -159,7 +232,7 @@ namespace asap.anim
             }
             else
             {
-                if (forward)
+                if (mode == FRAME_PROCESS_FORWARD)
                 {
                     Debug.Assert(placeObject.HasCharacter());
                     Debug.Assert(placeObject.GetMatrix() != null);
@@ -187,14 +260,14 @@ namespace asap.anim
                         displayList.AddChild(instance);
                     }
                 }
-                else // forward
+                else if (mode == FRAME_PROCESS_CLEAR)
                 {
                     displayList.ReplaceChildAt(CharacterInstance.NULL, index);
                 }
             }
         }
         
-        private void DoRemoveObject(Tag tag, bool forward)
+        private void DoRemoveObject(Tag tag, int mode)
         {
             int depth;
             switch (tag.GetCode())
@@ -209,11 +282,11 @@ namespace asap.anim
                     throw new NotImplementedException(tag.GetCode().ToString());
             }
             int index = depth - 1;
-            if (forward)
+            if (mode == FRAME_PROCESS_FORWARD)
             {
                 displayList.ReplaceChildAt(CharacterInstance.NULL, index);
             }
-            else
+            else if (mode == FRAME_PROCESS_CLEAR)
             {
                 CharacterInstance cachedInstance = instanceCache.FindAddedAtDepthBeforeFrame(depth, currentFrame);
                 Debug.Assert(cachedInstance != null);
@@ -265,53 +338,55 @@ namespace asap.anim
             set { frames = value; }
         }
 
+        private SWFFrame GetCurrentFrame()
+        {
+            return Frames[currentFrame];
+        }
+
         public void GotoAndPlay(int frameIndex)
         {
-            throw new NotImplementedException();
+            GotoHelper(frameIndex);
+            Play();
         }
 
         public void GotoAndStop(int frameIndex)
         {
-            throw new NotImplementedException();
+            GotoHelper(frameIndex);
+            Stop();
+        }
+
+        private void GotoHelper(int frameIndex)
+        {
+            Debug.Assert(frameIndex >= 0 && frameIndex < FramesCount);
+
+            if (frameIndex > currentFrame)
+            {
+                while (currentFrame < frameIndex)
+                    EnterNextFrame();
+            }
+            else if (frameIndex < currentFrame)
+            {
+                while (currentFrame > frameIndex)
+                    EnterPrevFrame();
+            }
         }
 
         public void NextFrame()
         {
-            currentFrame++;
-
-            Tag[] tags = Frames[currentFrame].Tags;
-            for (int i = 0; i < tags.Length; ++i)
-            {
-                ProcessTag(tags[i], true);                
-            }            
-
-            if (currentFrame == framesCount - 1)
-            {
-                OnAnimationFinished();
-            }
+            state = PlayerState.STEP;
+            EnterNextFrame();
         }
 
         public void PrevFrame()
         {
-            currentFrame--;
-
-            Tag[] tags = Frames[currentFrame].Tags;
-            for (int i = tags.Length - 1; i >= 0; --i)
-            {
-                ProcessTag(tags[i], false);                
-            }
-
-            if (currentFrame == 0)
-            {
-                OnAnimationFinished();
-            }
+            state = PlayerState.STEP;
+            EnterPrevFrame();
         }
 
         public void Play()
-        {
-            Reset();
+        {            
             state = PlayerState.PLAYING;
-            NextFrame();
+            EnterFrame();
         }
 
         public void Stop()
